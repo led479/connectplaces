d349d6bc9616455666bfaa1fcd7da8c3d9d03067
Rolf Zambon <led479.rs@gmail.com>
2018-09-17 18:04:19 -0300

    formatada a página de criar pessoa

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pessoas/index', as: 'pessoas'
+  get 'pessoas', to: 'pessoas#index'
   root 'paginas_estaticas#index'
   resources :pessoas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
