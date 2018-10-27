3887e8aa6f738ab3c2642d13ce970d70f1f44edd
Rolf Zambon <led479.rs@gmail.com>
2018-10-27 14:51:49 -0300

    Corrige as rotas de lugares

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   root 'paginas_estaticas#index'
-#  resources :pessoas
   resources :pessoas do
-    resources :lugares
+    resources :lugares, only: :index
   end
+  resources :lugares
+
   get 'lugares/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
